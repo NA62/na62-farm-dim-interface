e0fddc0bd8f0d4c2b632027bb7c0908aea1c68e6
Jonas Kunze <kunze.jonas@gmail.com>
2014-09-19 11:13:44 +0200

    cleanup

--- src/MessageQueueConnector.cpp
+++ src/MessageQueueConnector.cpp
@@ -47,7 +47,7 @@ void MessageQueueConnector::run() {
 
 				while (!(statisticsMessage =
 						IPCHandler::tryToReceiveStatistics()).empty()) {
-					if (Options::GetInt(OPTION_VERBOSITY) != 0) {
+					if (Options::GetInt(OPTION_VERBOSITY) > 1) {
 						std::cout << "Received: " << statisticsMessage
 								<< std::endl;
 					}
